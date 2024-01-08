8656bdef3858f4ce23326c791bb806c9ec6848a7
sakshibharath <151045201+sakshibharath@users.noreply.github.com>
2024-01-08 16:03:33 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,9 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.print("The value of a is 1");
-		System.out.print("The value of b is 2");
-		System.out.print("The output is found to be:"+(1+2));
+		System.out.println("The value of a is 1");
+		System.out.println("The value of b is 2");
+		System.out.println("The output is found to be:"+(1+2));
 
 	}
 
