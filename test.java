eae23ab52cafdd204bb87101bde0d44780bde067
sakshibharath <151045201+sakshibharath@users.noreply.github.com>
2024-01-08 16:01:35 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
-
 public class test {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the value of a:");
-		int a=sc.nextInt();
-		System.out.print("Enter the value of b:");
-		int b=sc.nextInt();
-		System.out.print("The output is found to be:"+(a+b));
+		System.out.print("The value of a is 1");
+		System.out.print("The value of b is 2");
+		System.out.print("The output is found to be:"+(1+2));
 
 	}
 
