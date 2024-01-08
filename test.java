cf30bffa2dc2260b8345e7daf3e7ada841bdfc4c
sakshibharath <151045201+sakshibharath@users.noreply.github.com>
2024-01-08 16:02:31 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,6 @@
 public class test {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
 		System.out.print("The value of a is 1");
 		System.out.print("The value of b is 2");
 		System.out.print("The output is found to be:"+(1+2));
